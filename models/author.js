7c2ad912d3a5cee9ad9c044d6d9bdf2d0228d14b
Surviantoro Ilham <survi@billowsoftware.com.au>
2020-04-24 13:10:39 +1000

    first commit

---
+++ models/author.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const authorSchema = new mongoose.Schema({
+ id: String,
+ first_name: String,
+ last_name: String
+});
+const Author = mongoose.model("author", authorSchema, "author");
+module.exports = Author;
